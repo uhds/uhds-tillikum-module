bcf67c817d8cadae1fe320c21afea3569be97cc8
Kevin <kevin.pack@oregonstate.edu>
2014-06-19 13:45:44 -0700

    Fixed EOL conversions, Changed joins to fetch-joins Remove uneeded joins when selecting applications
    Removing unneeded joins from fall 2014 returner datatable
    
    Fixed EOL conversion
    
    Adding completion and sections JOIN back to fetchApplications
    
    Change joins to fetch joins when selecting applications

--- src/TillikumX/Controller/Action/Helper/DataTablePendingFall2014.php
+++ src/TillikumX/Controller/Action/Helper/DataTablePendingFall2014.php
@@ -1,1 +1,103 @@
-<?phpnamespace TillikumX\Controller\Action\Helper;use DateTime;use Doctrine\ORM\EntityManager;class DataTablePendingFall2014 extends DataTablePendingHousingApplication{    const TEMPLATE_ID = 1;    public function dataTablePendingFall2014New()    {        return parent::dataTablePendingHousingApplication();    }    public function direct()    {        return $this->dataTablePendingFall2014New();    }    public function getBookingStartDate()    {        return new DateTime('2014-09-23');    }    public function getBookingEndDate()    {        return new DateTime('2015-06-12');    }    public function fetchApplications()    {        $ac = $this->_actionController;        $sm = $ac->getServiceManager();        $uhdsEm = $sm->get('doctrine.entitymanager.orm_uhds');        return $uhdsEm->createQuery(            '            SELECT a            FROM Uhds\Entity\HousingApplication\Application\Application a            LEFT JOIN a.completions c            LEFT JOIN a.sections s            WHERE a.template = :templateId AND a.state = :state            AND NOT EXISTS (SELECT r                                FROM Uhds\Entity\HousingApplication\RoomSelection\Reservation r                                WHERE r.confirmedBy IS NOT NULL AND r.removedBy IS NULL                                AND a = r.application                                )            '        )            ->setParameter('templateId', self::TEMPLATE_ID)            ->setParameter('state', 'processed')            ->getResult();    }    public function getBookingData($application)    {        return [            'start' => '2014-09-23',            'end' => '2015-06-12',            'billing' => [                'rates' => [                    [                        'delete_me' => false,                        'start' => '2014-09-28',                        'end' => '2014-12-12',                    ]                ],            ],        ];    }    public function getMealplanData($application)    {        $ac = $this->_actionController;        $sm = $ac->getServiceManager();        $tillikumEm = $sm->get('doctrine.entitymanager.orm_default');        $diningSection = $application->getSection('Dining');        $plan = null;        if ($diningSection) {            $plan = $tillikumEm->find(                'Tillikum\Entity\Mealplan\Mealplan',                $diningSection->getPlanId()            );        }        return [            'mealplan_id' => $plan->id,            'start' => '2014-09-28',            'end' => '2014-12-12',            'billing' => [                'rates' => [                    [                        'delete_me' => false,                        'rule_id' => $plan->default_billing_rule ? $plan->default_billing_rule->id : '',                        'start' => '2014-09-28',                        'end' => '2014-12-12',                    ]                ],            ],        ];    }}
+<?php
+
+namespace TillikumX\Controller\Action\Helper;
+
+use DateTime;
+use Doctrine\ORM\EntityManager;
+
+class DataTablePendingFall2014 extends DataTablePendingHousingApplication
+{
+    const TEMPLATE_ID = 1;
+
+    public function dataTablePendingFall2014New()
+    {
+        return parent::dataTablePendingHousingApplication();
+    }
+
+    public function direct()
+    {
+        return $this->dataTablePendingFall2014New();
+    }
+
+    public function getBookingStartDate()
+    {
+        return new DateTime('2014-09-23');
+    }
+
+    public function getBookingEndDate()
+    {
+        return new DateTime('2015-06-12');
+    }
+
+    public function fetchApplications()
+    {
+        $ac = $this->_actionController;
+        $sm = $ac->getServiceManager();
+
+        $uhdsEm = $sm->get('doctrine.entitymanager.orm_uhds');
+
+        return $uhdsEm->createQuery(
+            '
+            SELECT a, c, s
+            FROM Uhds\Entity\HousingApplication\Application\Application a
+            LEFT JOIN a.completions c
+            LEFT JOIN a.sections s
+            WHERE a.template = :templateId AND a.state = :state
+            '
+        )
+            ->setParameter('templateId', self::TEMPLATE_ID)
+            ->setParameter('state', 'processed')
+            ->getResult();
+    }
+
+    public function getBookingData($application)
+    {
+        return [
+            'start' => '2014-09-23',
+            'end' => '2015-06-12',
+            'billing' => [
+                'rates' => [
+                    [
+                        'delete_me' => false,
+                        'start' => '2014-09-28',
+                        'end' => '2014-12-12',
+                    ]
+                ],
+            ],
+        ];
+    }
+
+    public function getMealplanData($application)
+    {
+        $ac = $this->_actionController;
+        $sm = $ac->getServiceManager();
+
+        $tillikumEm = $sm->get('doctrine.entitymanager.orm_default');
+
+        $diningSection = $application->getSection('Dining');
+
+        $plan = null;
+        if ($diningSection) {
+            $plan = $tillikumEm->find(
+                'Tillikum\Entity\Mealplan\Mealplan',
+                $diningSection->getPlanId()
+            );
+        }
+
+        return [
+            'mealplan_id' => $plan->id,
+            'start' => '2014-09-28',
+            'end' => '2014-12-12',
+            'billing' => [
+                'rates' => [
+                    [
+                        'delete_me' => false,
+                        'rule_id' => $plan->default_billing_rule ? $plan->default_billing_rule->id : '',
+                        'start' => '2014-09-28',
+                        'end' => '2014-12-12',
+                    ]
+                ],
+            ],
+        ];
+    }
+}
--- src/TillikumX/View/Helper/DataTablePendingFall2014Returners.php
+++ src/TillikumX/View/Helper/DataTablePendingFall2014Returners.php
@@ -1,1 +1,23 @@
-<?php/** * The Tillikum Project (http://tillikum.org/) * * @link       http://tillikum.org/websvn/ * @copyright  Copyright 2009-2012 Oregon State University (http://oregonstate.edu/) * @license    http://www.gnu.org/licenses/gpl-2.0-standalone.html GPLv2 */namespace TillikumX\View\Helper;class DataTablePendingFall2014Returners extends DataTablePendingHousingApplication{    public function dataTablePendingFall2014Returners($rows)    {        return parent::dataTablePendingHousingApplication($rows);    }    public function getCaption()    {        return 'Pending fall 2014 returners housing applications';    }}
+<?php
+/**
+ * The Tillikum Project (http://tillikum.org/)
+ *
+ * @link       http://tillikum.org/websvn/
+ * @copyright  Copyright 2009-2012 Oregon State University (http://oregonstate.edu/)
+ * @license    http://www.gnu.org/licenses/gpl-2.0-standalone.html GPLv2
+ */
+
+namespace TillikumX\View\Helper;
+
+class DataTablePendingFall2014Returners extends DataTablePendingHousingApplication
+{
+    public function dataTablePendingFall2014Returners($rows)
+    {
+        return parent::dataTablePendingHousingApplication($rows);
+    }
+
+    public function getCaption()
+    {
+        return 'Pending fall 2014 returners housing applications';
+    }
+}
